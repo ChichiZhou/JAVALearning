a755c7a9b4dde3250910d54087b179112af3f1f4
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-02-05 23:25:06 -0600

    Add Function and Predicate

---
+++ src/main/java/LambdaFunction/FunctionProject.java
@@ -0,0 +1,41 @@
+package LambdaFunction;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class FunctionProject {
+    public static void main(String[] args) {
+        /**
+         * Consumer 就是干！给啥吃啥但是不吐
+         * Predicate 就是看！给啥吃了 但是就告诉你好不好吃
+         * Function 就是拉！给啥吃了 但是拉出来不一样的
+         */
+        Function<String, String> function = (a) -> {return a.toUpperCase();}; // 或者写成 (a) -> a.toUpperCaer();
+        System.out.println("\n************\n");
+
+        Function<List<Person>, Map<String, Double>> functionPerson = personList -> {
+            Map<String, Double> map = new HashMap<String, Double>();
+            personList.forEach(per -> map.put(per.getName(), per.getSalary()));
+            return map;                     // 这里要有 return
+        };
+
+        List<Person> personList = PersonRepository.getAllPersons();
+        Map<String, Double> map = functionPerson.apply(personList);       // 这里用 apply 才能调用 function
+        System.out.println("\n************\n");
+
+        /**
+         * 所有放参数的位置，都可以放变量 或者 集合 或者 别的东西
+         */
+        BiFunction<String, String, String> biFunction = (a, b) -> (a + " " + b);
+        System.out.println("\n************\n");
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/LambdaFunction/PredictProject.java
@@ -0,0 +1,23 @@
+package LambdaFunction;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredictProject {
+    public static void main(String[] args) {
+        Predicate<Integer> lessThan = a -> a >= 50;
+        lessThan.test(10);// 返回值是一个 boolean
+        System.out.println("\n************\n");
+
+        lessThan.and(lessThan).or(lessThan).negate().test(50);  // 注意这里和 consumer 一样，都只能接受同一个 test 的输入
+
+        System.out.println("\n************\n");
+        List<Person> personList = PersonRepository.getAllPersons();
+        Predicate<Person> personHeight = (p) -> p.getHeight() >= 100;
+        personList.stream().filter(personHeight).collect(Collectors.toList());   // stream 的 filter 是要接受一个 Predicate 的
+
+        System.out.println("\n************\n");
+
+    }
+}
