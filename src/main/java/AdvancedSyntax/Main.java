219f0ad5bb3de72473721c724404d0bba164f01c
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-02-07 00:16:43 -0600

    Generic method`

--- src/main/java/AdvancedSyntax/Main.java
+++ src/main/java/AdvancedSyntax/Main.java
@@ -15,7 +15,8 @@ public class Main {
         System.out.println("-----------------演示一下show_2-----------------");
         o.show_2(fruit);
         o.show_2(apple);
-        o.<Dog>show_2(dog);
+        o.<Dog>show_2(dog);  // 这里通过传入一个类型参数来改变原本这个类所在的类型参数
+                            // 这种写法在 Stream, Operation 中都会见到。总之所有的 泛型都会见到
         System.out.println("-----------------演示show_3-------------------");
         o.show_3(fruit);
         o.show_3(apple);
