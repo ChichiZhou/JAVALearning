a3d4d322c33eb74a8ebe75d25b0a18d1bb6b3244
He Zhou <hezho@amazon.com>
2020-02-10 17:34:17 -0600

    Switch syntax 1) Why is the change being made? ---------------------------------
    2) What has changed?
    ---------------------------------
    
    3) Other concerns and/or dependent changes?
    ---------------------------------
    
    4) Related sprint issue, TT, and/or MCM (if exist)
    ---------------------------------
    
    5) Related design/durability review/ORR document link (if applicable)
    ---------------------------------
    
    6) Testing
    ---------------------------------

--- src/main/java/LambdaFunction/Main.java
+++ src/main/java/LambdaFunction/Main.java
@@ -1,5 +1,7 @@
 package LambdaFunction;
 
+import static java.lang.String.format;
+
 import java.util.function.Consumer;
 
 public class Main {
@@ -9,5 +11,20 @@ public class Main {
         Consumer<String> con2 = (s) -> System.out.println(s.toLowerCase());
         //con2.accept("java8");
         con1.andThen(con2).accept("java8");
+
+        testSwitch("null");
+    }
+
+    public static void testSwitch(String a){
+        switch (a){
+        case "1":
+            System.out.println("1");
+            break;
+        case "null":
+            return;
+        default:
+            throw new IllegalArgumentException("No one wants u");
+        }
+        System.out.println("alway out?");
     }
 }
