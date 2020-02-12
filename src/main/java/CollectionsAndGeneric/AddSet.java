307c6aec3a9eb781ea687d1a783dce6552c77348
He Zhou <hezho@amazon.com>
2020-02-12 12:38:52 -0600

    How to create Set 1) Why is the change being made? ---------------------------------
    2) What has changed?
    ---------------------------------
    
    3) Other concerns and/or dependent changes?
    ---------------------------------
    
    4) Related sprint issue, TT, and/or MCM (if exist)
    ---------------------------------
    
    5) Related design/durability review/ORR document link (if applicable)
    ---------------------------------
    
    6) Testing
    ---------------------------------

--- src/main/java/CollectionsAndGeneric/AddSet.java
+++ src/main/java/CollectionsAndGeneric/AddSet.java
@@ -1,6 +1,7 @@
 package CollectionsAndGeneric;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -10,5 +11,12 @@ public class AddSet {
          * 创建一个Set
          */
         Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1,2,3,4,5));
+
+        Set<Integer> set1 = new HashSet<Integer>(){                      // 这种方法感觉有点蠢了
+            {
+                add(1);
+                add(2);
+            }
+        };
     }
 }
