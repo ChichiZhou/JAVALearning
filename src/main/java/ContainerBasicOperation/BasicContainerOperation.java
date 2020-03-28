18dae3712845cd066ecc8a00d8a1242f9b510b84
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-03-28 17:56:45 -0500

    Add one file

---
+++ src/main/java/ContainerBasicOperation/BasicContainerOperation.java
@@ -0,0 +1,15 @@
+package ContainerBasicOperation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicContainerOperation {
+    /**
+     * 如何创建 map
+     * @param args
+     */
+    public static void main(String[] args) {
+        Map<Integer, String> map1 = new HashMap<>();
+        // 向 map 中放入元素
+    }
+}
