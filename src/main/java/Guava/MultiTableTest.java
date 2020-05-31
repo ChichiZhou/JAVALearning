b6f0a1478d1abca821ad871203acdff1abba313f
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-05-31 17:24:38 -0500

    Add comments for table

--- src/main/java/Guava/MultiTableTest.java
+++ src/main/java/Guava/MultiTableTest.java
@@ -8,9 +8,15 @@ public class MultiTableTest {
         ImmutableTable<Integer, Integer, Integer> table = ImmutableTable.<Integer, Integer, Integer>builder()
                 .put(1, 2, 2)
                 .put(1,3,3)
+                .put(2,1,1)
+                .put(2,2,2)
                 .build();
 
+        // rowMap 只是把 row 当做 key，然后把剩余的组成 key value pair 的 Map，作为 value
         System.out.println("The rowMap is");
+        System.out.println(table.rowMap());
+        System.out.println("The rowMap.values() is");
+        // rowMap.values() 是把 rowMap 中的所有当做 value 的Map取出来
         System.out.println(table.rowMap().values());
 
 
