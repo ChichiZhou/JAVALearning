d59ff467727f44d1b61dfc121fad2be8546e0073
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-06-08 11:03:27 -0500

    Addmore

--- src/main/java/LambdaFunction/IteratorProject.java
+++ src/main/java/LambdaFunction/IteratorProject.java
@@ -1,5 +1,30 @@
 package LambdaFunction;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.*;
+
 public class IteratorProject {
+    public static void main(String[] args) {
+        Set<String> testSet = new HashSet<>();
+        testSet.add("1");
+        testSet.add("2");
+
+        Iterator iterator = testSet.iterator();
+
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+        Map<String, String> testMap = new HashMap<>();
+        testMap.put("1", "1");
+        testMap.put("2", "2");
+
+        Iterator iterator1 = testMap.entrySet().iterator();
+        while(iterator1.hasNext()){
+            Map.Entry<String, String> mapEntry = (Map.Entry)iterator1.next();
+            System.out.println(mapEntry.getKey());
+            System.out.println(mapEntry.getValue());
+        }
+    }
 
 }
